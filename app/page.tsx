033cfce538dff008d072116b10e6a8312b7a4053
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-16 11:17:08 +0530

    feat: enhance Rootpage layout with additional styling and buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,37 @@
-import { Box,Flex,Container,Heading,Text } from "@radix-ui/themes";
+import { Box,Flex,Container,Heading,Text, Button } from "@radix-ui/themes";
 
 const Rootpage= () => {
-  return <Box> 
+  return <Box style={{
+    marginTop:"128px",
+    marginBottom:"128px",
+  }}>
     <Container>
-      <Flex direction="column" align="center"></Flex>
-      <Heading align="center">React Framework for the web</Heading>
-        <Text align="center">Used by some of the world's largest companies, Next.js enables you to create high-quality web applications with the power of React components.</Text>
+      <Box style={{
+       border:"1px ",
+       borderStyle:"dashed",
+        borderColor:"gray.gray6",
+      }}> </Box>
+      <Flex direction="column" align="center" ></Flex>
+      <Heading align="center" size="9" style={{
+        padding:"24px"
+      }}>React Framework for the web</Heading>
+      <Box as="div"
+      style={{
+        borderBottom:"1px",
+        borderBottomStyle:"dashed",
+        borderBottomColor:"gray.gray6",
+        width:"100%",
 
+      }}></Box>
+        <Text align="center" style={{
+        padding:"24px"
+      }}>Used by some of the world's largest companies, Next.js enables you to create high-quality web applications with the power of React components.</Text>
+        <Flex direction="row" align="center" gap="4">
+          <Button>Get Started</Button>
+          <Button variant="soft">Learn</Button>
+          </Flex>
+         
     </Container>
   </Box>;
 };
-export default Rootpage;
+export default Rootpage;  
