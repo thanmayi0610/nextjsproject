f063e2f1f103e00efd14f1e4f7c184914832410d
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-16 11:29:23 +0530

    fix: update RootLayout metadata and reformat code structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
-import "./globals.css";
-import { PropsWithChildren } from "react";
-import { Metadata } from "next";
 import { Theme } from "@radix-ui/themes";
+import "./globals.css";
+
+import { Metadata } from "next";
+import { PropsWithChildren } from "react";
 import { ThemeProvider } from "./components/ThemeProvider";
 
-
 export const metadata: Metadata = {
-  title: "Root Layout",
-  description: "Root Layout",
-  };
-  const RootLayout = (props: PropsWithChildren) => {
-    return (
+  title: "Nickel",
+  description: "A crazy element",
+};
 
-      <html suppressHydrationWarning >
-        
-        <body>
+const RootLayout = (props: PropsWithChildren) => {
+  return (
+    <html suppressHydrationWarning>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        <Theme  accentColor="indigo">  {props.children}</Theme></ThemeProvider>
-        </body>
-        
-      </html>
-    );
-  }
-  export default RootLayout;
+          <Theme accentColor="indigo">{props.children}</Theme>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
